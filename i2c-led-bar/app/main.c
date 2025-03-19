670c1f1eb771fc049d0022bdd7e319965ea2ef55
ZaneMLewis <zanelewis67@gmail.com>
2025-03-18 21:40:51 -0600

    added patterns.h and patterns.c and edited main.c

--- i2c-led-bar/app/main.c
+++ i2c-led-bar/app/main.c
@@ -1,84 +1,43 @@
-/* --COPYRIGHT--,BSD_EX
- * Copyright (c) 2014, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *******************************************************************************
- *
- *                       MSP430 CODE EXAMPLE DISCLAIMER
- *
- * MSP430 code examples are self-contained low-level programs that typically
- * demonstrate a single peripheral function or device feature in a highly
- * concise manner. For this the code may rely on the device's power-on default
- * register values and settings such as the clock configuration and care must
- * be taken when combining code from several examples to avoid potential side
- * effects. Also see www.ti.com/grace for a GUI- and www.ti.com/msp430ware
- * for an API functional library-approach to peripheral configuration.
- *
- * --/COPYRIGHT--*/
-//******************************************************************************
-//  MSP430FR231x Demo - Toggle P1.0 using software
-//
-//  Description: Toggle P1.0 every 0.1s using software.
-//  By default, FR231x select XT1 as FLL reference.
-//  If XT1 is present, the PxSEL(XIN & XOUT) needs to configure.
-//  If XT1 is absent, switch to select REFO as FLL reference automatically.
-//  XT1 is considered to be absent in this example.
-//  ACLK = default REFO ~32768Hz, MCLK = SMCLK = default DCODIV ~1MHz.
-//
-//           MSP430FR231x
-//         ---------------
-//     /|\|               |
-//      | |               |
-//      --|RST            |
-//        |           P1.0|-->LED
-//
-//   Darren Lu
-//   Texas Instruments Inc.
-//   July 2015
-//   Built with IAR Embedded Workbench v6.30 & Code Composer Studio v6.1 
-//******************************************************************************
 #include <msp430.h>
+#include "patterns.h"
 
-int main(void)
-{
-    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+#define I2C_ADDRESS 0x45                //i2c address for LED bar
 
-    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
-    P1DIR |= BIT0;                          // Set P1.0 to output direction
+volatile unsigned char dataReceived[2]; //this provides a buffer for received I2C data
+volatile int dataRdy = 0;
 
-    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-                                            // to activate previously configured port settings
+void init_I2C_Target(void) {
+    UCB0CTLW0 = UCSWRST;                //puts eUSCI_B0 into a reset state
+    UCB0CTLW0 |= UCMODE_3 | UCSYNC;     //sets up synchronous i2c mode
+    UCB0I2COA0 = I2C_ADDRESS | UCOAEN;  //sets and enables address
+    UCB0CTLW0 &= ~UCSWRST;              
+    UCB0IE |= UCRXIE0;                  //enables the receive interrupt
+}
 
-    while(1)
-    {
-        P1OUT ^= BIT0;                      // Toggle P1.0 using exclusive-OR
-        __delay_cycles(100000);             // Delay for 100000*(1/MCLK)=0.1s
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD;           //stops the watchdog timer
+    PM5CTL0 &= ~LOCKLPM5;               //enables the GPIO
+
+    init_I2C_Target();
+    init_LED_Patterns();
+
+    __enable_interrupt();
+    
+    while (1) {
+        if (dataRdy) {
+            set_LED_Pattern(dataReceived[0]);
+            set_Phase_Time(dataReceived[1] * 6250);
+            dataRdy = 0;
+        }
+    }
+}
+
+#pragma vector = EUSCI_B0_VECTOR
+__interrupt void I2C_ISR(void) {
+    static int index = 0;
+    if (UCB0IFG & UCRXIFG0) {
+        dataReceived[index] = UCB0RXBUF;
+        index = (index + 1) % 2;
+        if (index == 0) dataRdy = 1;
     }
 }
